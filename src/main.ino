b7f9614bb15ff1de86ecebc0a4e29e370de0d5fb
Your Name <you@example.com>
2024-02-09 12:19:02 +0100

    fix

--- src/main.ino
+++ src/main.ino
@@ -12,14 +12,11 @@ float h = 0.0;
 
 
 void setup(){
-
   Serial.begin(115200);
   dht.begin();
-
 }
 
 void loop(){  
-
     float newT = dht.readTemperature();
     if (isnan(newT)) {
       Serial.println("Non riesco a leggere il sensore DHT!");
